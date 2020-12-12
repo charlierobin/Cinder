146c5ee445fd88ef2a7889ff83ca8f8f607512d5
Charlie <charlierobin@users.noreply.github.com>
2020-12-12 20:40:55 +0100

    adding extra blend modes to cinder::ip
    Just trying to start to learn a little bit about these things ... I’m new to all of this!

---
+++ src/cinder/ip/Color.cpp
@@ -0,0 +1,109 @@
+/*
+ Copyright (c) 2010, The Cinder Project, All rights reserved.
+
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/ip/Color.h"
+
+using namespace std;
+
+namespace cinder { namespace ip {
+
+template<bool DSTALPHA, bool DSTPREMULT, bool SRCPREMULT>
+void colorImpl_u8( Surface8u *background, const Surface8u &foreground, const Area &srcArea, ivec2 absOffset )
+{
+//	bool SRCALPHA = foreground.hasAlpha();
+	const ptrdiff_t srcRowBytes = foreground.getRowBytes();
+	const uint8_t sR = foreground.getChannelOrder().getRedOffset();
+	const uint8_t sG = foreground.getChannelOrder().getGreenOffset();
+	const uint8_t sB = foreground.getChannelOrder().getBlueOffset();
+//	const uint8_t sA = SRCALPHA ? (foreground.getChannelOrder().getAlphaOffset()) : 0;	
+	const uint8_t srcInc = foreground.getPixelInc();
+	const ptrdiff_t dstRowBytes = background->getRowBytes();
+	const uint8_t dR = background->getChannelOrder().getRedOffset();
+	const uint8_t dG = background->getChannelOrder().getGreenOffset();
+	const uint8_t dB = background->getChannelOrder().getBlueOffset();
+//	const uint8_t dA = DSTALPHA ? (background->getChannelOrder().getAlphaOffset()) : 0;
+	const uint8_t dstInc = background->getPixelInc();
+	const int32_t width = srcArea.getWidth();
+	
+	for( int32_t y = 0; y < srcArea.getHeight(); ++y ) {
+		
+		const uint8_t *src = reinterpret_cast<const uint8_t*>( reinterpret_cast<const uint8_t*>( foreground.getData() + srcArea.x1 * 4 ) + ( srcArea.y1 + y ) * srcRowBytes );
+		
+		uint8_t *dst = reinterpret_cast<uint8_t*>( reinterpret_cast<uint8_t*>( background->getData() + absOffset.x * 4 ) + ( y + absOffset.y ) * dstRowBytes );
+		
+		for( int32_t x = 0; x < width; ++x ) {
+		
+			// Creates a result color with the luminance of the base color and the hue and saturation of the blend color.
+			// This preserves the gray levels in the image and is useful for coloring monochrome images and for tinting color images.
+		
+			Color dstColour = Color( dst[dR] / 255.0, dst[dG] / 255.0, dst[dB] / 255.0 );
+			Color srcColour = Color( src[sR] / 255.0, src[sG] / 255.0, src[sB] / 255.0 );
+			
+			vec3 dstHSV = dstColour.get( CM_HSV );
+			vec3 srcHSV = srcColour.get( CM_HSV );
+			
+			vec3 finalHSV = vec3( srcHSV.x, srcHSV.y, dstHSV.z );
+			
+			dstColour.set( CM_HSV, finalHSV );
+			
+			dst[dR] = dstColour.r * 255.0;
+			dst[dG] = dstColour.g * 255.0;
+			dst[dB] = dstColour.b * 255.0;
+			
+			src += srcInc;
+			dst += dstInc;
+		}
+	}
+}
+
+void color( Surface8u *background, const Surface8u &foreground, const Area &srcArea, const ivec2 &dstRelativeOffset )
+{
+	pair<Area,ivec2> srcDst = clippedSrcDst( foreground.getBounds(), srcArea, background->getBounds(), srcArea.getUL() + dstRelativeOffset );	
+	if( background->hasAlpha() ) {
+		if( background->isPremultiplied() ) {
+			if( foreground.isPremultiplied() )
+				colorImpl_u8<true, true, true>( background, foreground, srcDst.first, srcDst.second );
+			else
+				colorImpl_u8<true, true, false>( background, foreground, srcDst.first, srcDst.second );
+		}
+		else { // background unpremult
+			if( foreground.isPremultiplied() )
+				colorImpl_u8<true, false, true>( background, foreground, srcDst.first, srcDst.second );
+			else
+				colorImpl_u8<true, false, false>( background, foreground, srcDst.first, srcDst.second );
+		}
+	}
+	else { // background no alpha
+		if( foreground.isPremultiplied() )
+			colorImpl_u8<false, false, true>( background, foreground, srcDst.first, srcDst.second );
+		else
+			colorImpl_u8<false, false, false>( background, foreground, srcDst.first, srcDst.second );
+	}
+}
+
+void color( Surface32f *background, const Surface32f &foreground, const Area &srcArea, const ivec2 &dstRelativeOffset )
+{
+	throw new Exception( "Not yet implemented" );
+}
+
+} } // namespace cinder::ip
---
+++ src/cinder/ip/Darken.cpp
@@ -0,0 +1,99 @@
+/*
+ Copyright (c) 2010, The Cinder Project, All rights reserved.
+
+ This code is intended for use with the Cinder C++ library: http://libcinder.org
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+ the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+	the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "cinder/ip/Darken.h"
+
+using namespace std;
+
+namespace cinder { namespace ip {
+
+template<bool DSTALPHA, bool DSTPREMULT, bool SRCPREMULT>
+void darkenImpl_u8( Surface8u *background, const Surface8u &foreground, const Area &srcArea, ivec2 absOffset )
+{
+	bool SRCALPHA = foreground.hasAlpha();
+	const ptrdiff_t srcRowBytes = foreground.getRowBytes();
+	const uint8_t sR = foreground.getChannelOrder().getRedOffset();
+	const uint8_t sG = foreground.getChannelOrder().getGreenOffset();
+	const uint8_t sB = foreground.getChannelOrder().getBlueOffset();
+	const uint8_t sA = SRCALPHA ? (foreground.getChannelOrder().getAlphaOffset()) : 0;	
+	const uint8_t srcInc = foreground.getPixelInc();
+	const ptrdiff_t dstRowBytes = background->getRowBytes();
+	const uint8_t dR = background->getChannelOrder().getRedOffset();
+	const uint8_t dG = background->getChannelOrder().getGreenOffset();
+	const uint8_t dB = background->getChannelOrder().getBlueOffset();
+	const uint8_t dA = DSTALPHA ? (background->getChannelOrder().getAlphaOffset()) : 0;
+	const uint8_t dstInc = background->getPixelInc();
+	const int32_t width = srcArea.getWidth();
+	
+	for( int32_t y = 0; y < srcArea.getHeight(); ++y ) {
+		
+		const uint8_t *src = reinterpret_cast<const uint8_t*>( reinterpret_cast<const uint8_t*>( foreground.getData() + srcArea.x1 * 4 ) + ( srcArea.y1 + y ) * srcRowBytes );
+		
+		uint8_t *dst = reinterpret_cast<uint8_t*>( reinterpret_cast<uint8_t*>( background->getData() + absOffset.x * 4 ) + ( y + absOffset.y ) * dstRowBytes );
+		
+		for( int32_t x = 0; x < width; ++x ) {
+		
+			// Looks at the color information in each channel and selects the base or blend color—whichever is darker—as the result color.
+			// Pixels lighter than the blend color are replaced, and pixels darker than the blend color do not change.
+			
+			if ( dst[dR] > src[sR] ) dst[dR] = src[sR];
+			if ( dst[dG] > src[sG] ) dst[dG] = src[sG];
+			if ( dst[dB] > src[sB] ) dst[dB] = src[sB];
+			
+			src += srcInc;
+			dst += dstInc;
+		}
+	}
+}
+
+void darken( Surface8u *background, const Surface8u &foreground, const Area &srcArea, const ivec2 &dstRelativeOffset )
+{
+	pair<Area,ivec2> srcDst = clippedSrcDst( foreground.getBounds(), srcArea, background->getBounds(), srcArea.getUL() + dstRelativeOffset );	
+	if( background->hasAlpha() ) {
+		if( background->isPremultiplied() ) {
+			if( foreground.isPremultiplied() )
+				darkenImpl_u8<true, true, true>( background, foreground, srcDst.first, srcDst.second );
+			else
+				darkenImpl_u8<true, true, false>( background, foreground, srcDst.first, srcDst.second );
+		}
+		else { // background unpremult
+			if( foreground.isPremultiplied() )
+				darkenImpl_u8<true, false, true>( background, foreground, srcDst.first, srcDst.second );
+			else
+				darkenImpl_u8<true, false, false>( background, foreground, srcDst.first, srcDst.second );
+		}
+	}
+	else { // background no alpha
+		if( foreground.isPremultiplied() )
+			darkenImpl_u8<false, false, true>( background, foreground, srcDst.first, srcDst.second );
+		else
+			darkenImpl_u8<false, false, false>( background, foreground, srcDst.first, srcDst.second );
+	}
+}
+
+void darken( Surface32f *background, const Surface32f &foreground, const Area &srcArea, const ivec2 &dstRelativeOffset )
+{
+	throw new Exception( "Not yet implemented" );
+}
+
+} } // namespace cinder::ip
